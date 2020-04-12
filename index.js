f803aed9e1069557e859205e26d6ac23a1bc07e1
thetseffect <thetseffect@gmail.com>
2020-04-12 16:32:18 +0530

    Added till middlewares

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { User } = require("./models/user");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true })
@@ -15,20 +20,46 @@ mongoose
     console.error(err);
   });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("HI");
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
 });
+
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-  user.save((err, userdata) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
+
+  user.save((err, doc) => {
+    if (err) res.json({ success: false, err });
+    res.status(200).json({
       success: true,
-      userData: userdata,
+      userData: doc,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth Failed, Email not Found",
+      });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (err)
+        return res.json({ loginSuccess: false, messgae: "Wrong Password" });
+      user.generateToken((err, token) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true });
+      });
     });
   });
 });
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+var jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,50 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  let user = this;
+  bcrypt.compare(plainPassword, user.password, function (err, isMatch) {
+    if (!isMatch) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  let user = this;
+  const token = jwt.sign(user._id.toHexString(), "secret");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  let user = this;
+  jwt.verify(token, "secret", function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
